047c4f73ef4dd4e1edc6d6a94cfba4c0bd4f829d
sanpax <109195847+sanpax@users.noreply.github.com>
2022-07-27 04:20:22 +0200

    Create config.php

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'occ6f7365735',
+  'passwordsalt' => '2c5778476346786306303',
+  'trusted_domains' =>
+  array (
+    0 => 'localhost',
+    1 => 'studio',
+  ),
+  'datadirectory' => '/var/www/nextcloud/data',
+  'dbtype' => 'sqlite3',
+  'version' => '7.0.2.1',
+  'installed' => true,
+);
